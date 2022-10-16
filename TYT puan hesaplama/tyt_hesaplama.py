b684ef3e92c7daa115eae58f9d8d58b9a8f9c438
Melihemrebykbs <buyukbasmelih6@gmail.com>
2022-10-16 19:35:48 +0300

    TYT puan hesaplama programı
    TYT puan hesaplama programı

---
+++ TYT puan hesaplama/tyt_hesaplama.py
@@ -0,0 +1,28 @@
+print("      TYT HESAPLMA      ")
+
+Türkçe =input("Türkçe net:")
+Sosyal =input("Sosyal Bilimler net:")
+Matematik =input("Temel Matematik net:")
+Fen = input("Fen Bilimleri net:")
+
+float=(Türkçenet) = int(Türkçe) * 2.92
+float=(Sosyanet) = int(Sosyal) * 2.98
+float=(Matematikenet) = int(Matematik) * 4.58
+float=(Fennet) = int(Fen) * 3.18
+
+"""
+print("Türkçe net:", Türkçenet)
+print("Sosyal Bilimler net:", Sosyanet)
+print("Temel Matematik ne net:", Matematikenet)
+print("Fen Bilimleri net:", Fennet)"""
+
+genelpuan = [Türkçenet + Sosyanet + Matematikenet + Fennet]
+
+print("Tyt Puan'nın:", genelpuan)
+
+
+
+
+
+
+
